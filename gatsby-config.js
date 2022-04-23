a142eba3992c80e40c7cfcd34b1806c31e865195
Tom <tom.lener@gmail.com>
2022-04-23 23:08:15 +0200

    tml

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+     resolve: `gatsby-plugin-google-fonts`,
+     options: {
+       fonts: [
+         `Josefin Sans\:300,400,700`,
+         `Lekton\:400,700`,
+         `Proza Libre\:400,400i,700,700i`,
+         `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+       ],
+       display: 'swap'
+     }
+   }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
